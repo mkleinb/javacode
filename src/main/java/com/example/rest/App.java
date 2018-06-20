e34a8ada275fbc13baa39928dc1183a8b7a1cd0a
mkleinb <mkleinb@icloud.com>
2018-06-20 20:33:46 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://ip.jsontest.com/";
+        String url = "http://129.157.179.180:3000/shield/33/45/blue/mkleinb";
         CloseableHttpResponse response = null;
  
         try {
